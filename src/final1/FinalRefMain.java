8061b4a42aed718f44486c56a06b0f7085f21000
Minjeong Kang <jennygh2@hanmail.net>
2024-02-01 16:56:13 +0900

    정리

--- src/final1/FinalRefMain.java
+++ src/final1/FinalRefMain.java
@@ -11,5 +11,8 @@ public class FinalRefMain {
         data.value = 20;
         System.out.println(data.value);
 
+        //정리
+        //참조형 변수에 final이 붙으면 참조 대상 자체를 다른 대상으로 변경하지 못하는 것이지 참조하는 대상의 값은 변경할 수 있다.
+
     }
 }
