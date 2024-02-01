e26f323b9c81c4fbcf28e8f7f51f8153c4d50d53
Minjeong Kang <jennygh2@hanmail.net>
2024-02-01 13:27:49 +0900

    static 변수 3

--- src/static1/Data3.java
+++ src/static1/Data3.java
@@ -2,6 +2,9 @@ package static1;
 
 public class Data3 {
 
+    //멤버 변수의 종류
+    //1. 인스턴스 변수: static이 붙지 않은 멤버 변수로 인스턴스를 생성해야 사용할 수 있고, 인스턴스를 만들 때 마다 새로 만들어진다.
+    //2. 클래스 변수: static이 붙은 멤버 변수로 인스턴스와 무관하게 클래스에 바로 접근해서 사용할 수 있고, 클래스 자체에 소속되어 있다. 딱 1개만 만들어지고 여러곳에서 공유하는 목적으로 사용된다.
     public String name;
     public static int count; //static: 정적 변수, 클래스 변수
 
--- src/static1/DataCountMain3.java
+++ src/static1/DataCountMain3.java
@@ -16,5 +16,13 @@ public class DataCountMain3 {
         //static 변수는 쉽게 이야기해서 클래스인 붕어빵 틀이 특별히 관리하는 변수이다. 붕어빵 틀은 1개이므로 클래스 변수도 하나만 존재한다.
         //반면에 인스턴스인 붕어빵은 인스턴스의 수 만큼 변수가 존재한다.
 
+        //추가
+        //인스턴스를 통한 접근은 가능하나 권장하지 않는다. 코드를 읽을 때 마치 인스턴스 변수에
+        Data3 data4 = new Data3("D");
+        System.out.println("D count = " + data4.count);
+
+        //클래스를 통한 접근
+        System.out.println(Data3.count);
+
     }
 }
