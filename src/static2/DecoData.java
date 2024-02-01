5f95cebfcb666b14e325a3e9b4a50c2c84b97e86
Minjeong Kang <jennygh2@hanmail.net>
2024-02-01 14:41:41 +0900

    static 메서드 2

---
+++ src/static2/DecoData.java
@@ -0,0 +1,36 @@
+package static2;
+
+public class DecoData {
+
+    private int instanceValue;
+    private static int staticValue;
+
+    public static void staticCall() {
+        //instanceValue++; //인스턴스 변수 접근, compile error
+        //instanceMethod(); //인스턴스 메서드 접근, compile error
+
+        staticValue++; //정적 변수에 접근
+        staticMethod(); //정적 메서드 접근
+    }
+
+    public void instanceCall() {
+        instanceValue++; //인스턴스 변수 접근
+        instanceMethod(); //인스턴스 메서드 접근
+
+        staticValue++; //정적 변수 접근, DecoData.staticValue++; 와 같다
+        staticMethod(); //정적 메서드 접근
+    }
+
+    public static void staticCall(DecoData data) { //외부에서 참조값 넘겨와서 가능
+        data.instanceValue++;
+        data.instanceMethod();
+    }
+
+    private void instanceMethod() {
+        System.out.println("instanceValue: " + instanceValue);
+    }
+
+    private static void staticMethod() {
+        System.out.println("staticValue: " + staticValue);
+    }
+}
