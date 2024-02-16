8a9d6673add4ec84143433247332d54119e00da3
Minjeong Kang <jennygh2@hanmail.net>
2024-02-16 15:20:04 +0900

    다형성 - 역할과 구현 예제 3

--- src/poly/car0/CarMain0.java
+++ src/poly/car0/CarMain0.java
@@ -10,6 +10,7 @@ public class CarMain0 {
         driver.drive();
 
         //추가: k3를 운전하던 운전자가 Model3로 차량을 변경해서 운전하는 코드이다.
+        //여기서 새로운 차량을 추가한다면 또 다시 Driver 코드를 많이 변경해야 한다. 만약 운전할 수 있는 차량의 종류가 계속 늘어난다면 점점 더 변경해야 하는 코드가 많아질 것이다.
         Model3Car model3Car = new Model3Car();
         driver.setK3Car(null); //기존 k3Car의 참조를 제거한다.
         driver.setModel3Car(model3Car); //새로운 model3Car의 참조를 추가한다.
